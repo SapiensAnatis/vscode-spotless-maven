2855088103c4074e4ac75cd7823295ae3e713192
Jay Malhotra <jay0@me.com>
2025-02-01 12:38:26 +0000

    Add Prettier

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,49 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import getPomPath from './maven/getPomPath';
+import Spotless from './spotless';
+import ChildProcessExecutor from './maven/childProcessExecutor';
+import DocumentFormattingEditProvider from './documentFormattingEditProvider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(
+  context: vscode.ExtensionContext
+): Promise<void> {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "vscode-spotless-maven" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-spotless-maven" is now active!');
+  const pomUri = await getPomPath();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-spotless-maven.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Spotless Maven!');
-	});
+  if (!pomUri) {
+    console.log('No Maven project found.');
+    return;
+  }
 
-	context.subscriptions.push(disposable);
+  console.log(`Found Maven project: ${pomUri}`);
+
+  const executor = new ChildProcessExecutor();
+  const spotless = new Spotless(executor);
+  const formatDocumentSelector: vscode.DocumentFilter[] = [
+    { language: 'java', scheme: 'file' },
+  ];
+
+  const documentFormattingEditProvider = new DocumentFormattingEditProvider(
+    spotless,
+    formatDocumentSelector
+  );
+
+  const formatter = vscode.languages.registerDocumentFormattingEditProvider(
+    'java',
+    documentFormattingEditProvider
+  );
+
+  context.subscriptions.push(formatter, executor);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): void {}
